032f2599062499b1f58207d164bd2052aa3a4a0b
Kiryous <ya@kirys.ru>
2017-04-17 11:08:13 +0300

    Add server.js for serve demo

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = new express();
+var path = require('path');
+var port = 8888;
+
+app.use('/',express.static(__dirname)); 
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve('index.html'));
+});
+
+var listener = app.listen(8888, function(){
+    console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+});

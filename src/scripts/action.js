83a03e46556fc98326471657235aa5a594c53fc9
elenaiumagulova <elenaiumagulova@gmail.com>
2017-05-27 21:47:45 +0300

    Made a swiper slider

--- src/scripts/action.js
+++ src/scripts/action.js
@@ -1,1 +1,15 @@
-
+$(document).ready(function () {
+   //initialize swiper when document ready
+  //  var mySwiper = new Swiper ('.swiper-container', {
+  //    loop: true,
+  //    direction: 'horizontal',
+  //    pagination: '.swiper-pagination',
+  //    paginationClickable: true
+  //  })
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      centeredSlides: true,
+  });
+  swiper.slideTo(1, 10, false);
+ });

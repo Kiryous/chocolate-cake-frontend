3f6f259af5a6ccfad270bba071ab9cb165ceaac7
elenaiumagulova <elenaiumagulova@gmail.com>
2017-06-03 14:05:53 +0300

    Fixed some errors

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -2,5 +2,7 @@ var $hamburger = $(".hamburger");
 $hamburger.on("click", function(e) {
   $hamburger.toggleClass("is-active").click(function(){
     $(this).toggleClass('open');
+    // jQuery(this).toggleClass('is-active');
+    // jQuery('.hamburger-inner ul').toggleClass('is-active');
   });
 });
--- src/scripts/scrolldown.js
+++ src/scripts/scrolldown.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-  $(#concertpage-button).click(function(){
-    var elementClick = $(this).attr("href");
-    var destination = $(elementClick).offset().top;
-    if ($.browser.safari) {
-        $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость прокрутки
-    } else {
-        $('html').animate({ scrollTop: destination }, 1100);
-    }
-    return false;
-  });
-});
+// $(document).ready(function(){
+//   $(#concertpage-button).click(function(){
+//     var elementClick = $(this).attr("href");
+//     var destination = $(elementClick).offset().top;
+//     if ($.browser.safari) {
+//         $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость прокрутки
+//     } else {
+//         $('html').animate({ scrollTop: destination }, 1100);
+//     }
+//     return false;
+//   });
+// });

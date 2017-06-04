660f4fc652f3b2a2143c16aec557edf88ff0c8ce
elenaiumagulova <elenaiumagulova@gmail.com>
2017-06-04 14:14:25 +0300

    Add adaptive for popup in shoppage.html

--- src/scripts/modalshopitem.js
+++ src/scripts/modalshopitem.js
@@ -1,25 +1,3 @@
-// $(document).ready(function() {
-// 	$('a#openModal').click( function(event){
-// 		event.preventDefault();
-// 		$('#overlay').fadeIn(400,
-// 		 	function(){
-// 				$('#modalWindow')
-// 					.css('display', 'block')
-// 					.animate({opacity: 1, top: '50%'}, 200);
-// 		});
-// 	});
-//
-// 	$('#closeModal, #overlay').click( function(){
-// 		$('#modalWindow')
-// 			.animate({opacity: 0, top: '45%'}, 200,
-// 				function(){
-// 					$(this).css('display', 'none');
-// 					$('#overlay').fadeOut(400);
-// 				}
-// 			);
-// 	});
-// });
-
 function show(state){
 		document.getElementById('modal-window').style.display = state;
 		document.getElementById('modal-wrap').style.display = state;
